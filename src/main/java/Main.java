162c63d733117b9b6ab7acbd40c6c9e1c8c14031
ddoyle2017 <ddoyle4@uic.edu>
2017-10-19 19:09:03 -0500

    added JDA dependencies to gradle and JDA API instatiation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,34 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+/*
+ * AvocadoBot Main Class
+ *
+ * Handles the instantiation of the bot through the JDA API, including
+ * the bot's login information and token.
+ *
+ */
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class Main
+import javax.security.auth.login.LoginException;
+
+
+public class Main extends ListenerAdapter
 {
-    public static void main (String[] args) throws java.lang.Exception
+    public static void main (String[] args) throws LoginException, RateLimitedException
     {
-        System.out.println("Hello World! I am AvocadoBot!");
+        JDA API = new JDABuilder(AccountType.BOT)
+                .setToken("MzY5NzI2NzUwMTM5MDg4ODk3.DMculg.pJQSj_5-0LJS6-6Wu137qFNaK9c")
+                .buildAsync();
+
+    }
+
+    //  Event listener
+    @Override
+    public void onMessageReceived (MessageReceivedEvent event)
+    {
+        System.out.println("I received a message in my butt");
     }
 }

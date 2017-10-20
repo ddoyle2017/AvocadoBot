3e64d1001f993bb009630180a070e91d9fe47ac5
ddoyle2017 <ddoyle4@uic.edu>
2017-10-20 15:31:41 -0500

    added audio player event handlers, need to be implemented

--- src/main/java/Music/TrackScheduler.java
+++ src/main/java/Music/TrackScheduler.java
@@ -1,6 +1,56 @@
 package Music;
 
-public class TrackScheduler
-{
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import javafx.scene.media.AudioTrack;
 
+/**
+ *  TrackScheduler Class
+ *
+ *  Event handler class for the AudioPlayer object. Handles user interactions like Play, Pause, Skip, etc.,
+ *  as well as keeping track of the song queue.
+ *
+ */
+public class TrackScheduler extends AudioEventAdapter
+{
+    @Override
+    public void onPlayerPause(AudioPlayer player)
+    {
+
+    }
+
+    @Override
+    public void onPlayerResume(AudioPlayer player)
+    {
+
+    }
+
+    @Override
+    public void onTrackStart(AudioPlayer player, AudioTrack track)
+    {
+
+    }
+
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason)
+    {
+        if (endReason.mayStartNext)
+        {
+            // play next track
+        }
+    }
+
+    @Override
+    public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException ex)
+    {
+
+    }
+
+    @Override
+    public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs)
+    {
+
+    }
 }

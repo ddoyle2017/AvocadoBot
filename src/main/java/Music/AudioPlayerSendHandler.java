0e8a06372c36f548c6c7de6cf01f2562e551778a
ddoyle2017 <ddoyle4@uic.edu>
2017-10-20 16:05:54 -0500

    implemented audio player manager

--- src/main/java/Music/AudioPlayerSendHandler.java
+++ src/main/java/Music/AudioPlayerSendHandler.java
@@ -1,6 +1,45 @@
 package Music;
 
-public class AudioPlayerSendHandler
-{
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
+import net.dv8tion.jda.core.audio.AudioSendHandler;
 
+
+public class AudioPlayerSendHandler implements AudioSendHandler
+{
+    private final AudioPlayer audioPlayer;
+    private AudioFrame lastFrame;
+
+    public AudioPlayerSendHandler()
+    {
+        this.audioPlayer = audioPlayer;
+    }
+
+    @Override
+    public boolean canProvide()
+    {
+        if (lastFrame == null)
+        {
+            lastFrame = audioPlayer.provide();
+        }
+
+        return lastFrame != null;
+    }
+
+    @Override
+    public byte[] provide20MsAudio()
+    {
+        if (lastFrame == null)
+        {
+            lastFrame = audioPlayer.provide();
+        }
+
+        return lastFrame.data;
+    }
+
+    @Override
+    public boolean isOpus()
+    {
+        return true;
+    }
 }
--- src/main/java/Music/Music.java
+++ src/main/java/Music/Music.java
@@ -1,6 +1,20 @@
 package Music;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+
 public class Music
 {
+    private final AudioPlayerManager playerManager;
 
+    public Music()
+    {
+        this.playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+    }
+
+    public AudioPlayerManager getPlayerManager() {
+        return playerManager;
+    }
 }

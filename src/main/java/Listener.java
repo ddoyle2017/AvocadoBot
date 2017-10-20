783f1a5621c71fdff73f05ad94b64ffe1e09bc7d
ddoyle2017 <ddoyle4@uic.edu>
2017-10-20 03:12:49 -0500

    added Music package with TrackScheduler and AudioPlayerSendHandler classes

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -15,16 +15,11 @@ public class Listener extends ListenerAdapter
     @Override
     public void onMessageReceived (MessageReceivedEvent event)
     {
-        //  For now, we don't want bot-to-bot communication. Check event object to see if the communication
-        //  is from a bot. If so, return nothing.
-        if (event.getAuthor().isBot()) return;
+        if (event.getAuthor().isBot()) return;        //  is from a bot. If so, return nothing.
 
-        // Remember that Discord communication is done via JSON files. We need to get the appropriate string from the
-        // object that was created from the JSON file (the event class)
         Message message = event.getMessage();
         String content  = message.getRawContent();  // getRawContent is an atomic getter that keeps discord formatting
 
-        // handle the command
         if (content.equals("!avocado"))
         {
             MessageChannel channel = event.getChannel(); // get which channel the message came from

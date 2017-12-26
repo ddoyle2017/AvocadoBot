f3e85a4261a44523141865215adb229792c646fe
Daniel Doyle <ddoyle4@uic.edu>
2017-12-25 22:14:23 -0600

    Bot can now join and leave voice channels. Added this feature to the play, join, and leave commands.

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -1,19 +1,15 @@
-import javafx.scene.paint.Color;
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.entities.ChannelType;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.audio.hooks.ConnectionStatus;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-
-import java.util.StringTokenizer;
+import net.dv8tion.jda.core.managers.AudioManager;
 
 /**
  * Listener Class
  *
- * -All event listeners go here. This class grabs any messages received via Discord, parses the strings, then
- * handles the commands.
+ * - All event listeners go here. This class grabs any messages received via Discord, parses the strings, then
+ *   handles the commands.
  *
  */
 public class Listener extends ListenerAdapter
@@ -42,10 +38,21 @@ public class Listener extends ListenerAdapter
         else if (content.startsWith("!avocado play") || content.startsWith("!a play"))
         {
             channel.sendMessage(":notes: **Playing music**").queue();
+            joinVoiceChannel(event, channel);
 
-            //
-            // Here is where we start working with LavaPlayer
-            //
+            ////////////////////////////////////////////////////
+            //                                                //
+            // Here is where we start working with LavaPlayer //
+            //                                                //
+            ////////////////////////////////////////////////////
+        }
+        else if (content.equals("!avocado join") || content.equals("!a join"))
+        {
+            joinVoiceChannel(event, channel);
+        }
+        else if (content.equals("!avocado leave") || content.equals("!a leave"))
+        {
+            leaveVoiceChannel(event, channel);
         }
     }
 
@@ -53,9 +60,38 @@ public class Listener extends ListenerAdapter
     {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setColor(java.awt.Color.GREEN);
-        embedBuilder.addField("!a play [URL | Query]", "Give AvocadoBot a song or YouTube link to play\n", false);
-        embedBuilder.addField("!a help", "Provides a list of commands\n", false);
-        embedBuilder.addField("!a hook [URL]", "Creates a webhook for updates from the given link", false);
+        embedBuilder.addField("!a play [URL | Query]", "Give AvocadoBot a song or YouTube link to play.\n", false);
+        embedBuilder.addField("!a join", "AvocadoBot will join your voice channel.\n", false);
+        embedBuilder.addField("!a leave", "Disconnects AvocadoBot from your voice channel.\n", false);
+        embedBuilder.addField("!a help", "Provides a list of commands.\n", false);
+        embedBuilder.addField("!a hook [URL]", "Creates a webhook for updates from the given link.", false);
         return embedBuilder.build();
+    }
+
+    private void joinVoiceChannel(MessageReceivedEvent event, MessageChannel channel)
+    {
+        VoiceChannel voiceChannel = event.getMember().getVoiceState().getChannel();
+        AudioManager manager = event.getGuild().getAudioManager(); // a Discord server is called a "Guild"
+
+        if (voiceChannel == null)
+        {
+            channel.sendMessage(":x: **You have to be in a voice channel to use this command**").queue();
+        }
+        else if (manager.getConnectionStatus() != ConnectionStatus.CONNECTED)
+        {
+            manager.openAudioConnection(voiceChannel);
+            channel.sendMessage(":ok_hand: **Joined** `" + voiceChannel.getName() + "`").queue();
+        }
+    }
+
+    private void leaveVoiceChannel(MessageReceivedEvent event, MessageChannel channel)
+    {
+        AudioManager manager = event.getGuild().getAudioManager();
+
+        if (manager.getConnectionStatus() == ConnectionStatus.CONNECTED)
+        {
+            manager.closeAudioConnection();
+            channel.sendMessage(":last_quarter_moon_with_face: **Successfully disconnected**").queue();
+        }
     }
 }

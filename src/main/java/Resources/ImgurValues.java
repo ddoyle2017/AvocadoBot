8eb19473545e79ebba410a8d6e997a4919e67363
Daniel Doyle <ddoyle4@uic.edu>
2018-02-16 14:57:11 -0600

    added image commands to the help menu

--- src/main/java/Resources/ImgurValues.java
+++ src/main/java/Resources/ImgurValues.java
@@ -4,10 +4,8 @@ public final class ImgurValues
 {
     private ImgurValues() {}
 
-    public static final String BASE_IMGUR_URL = "http://i.imgur.com/";
     public static final String IMGUR_API_URL = "https://api.imgur.com/3/";
     public static final String SEARCH_IMGUR = "gallery/search/hot/all/0/?q=";
-    public static final String GRAB_ALBUM = "album/";
     public static final String GRAB_NEWEST_SLASHW_ALBUM = "gallery/r/slashw/newest/day/1";
     public static final String AS_JSON = ".json";
     public static final String CANT_FIND_AUTH_FILE = "Can't find authentication info";
